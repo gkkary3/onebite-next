67f93662eff8b83a93c170be4ce07d2a07f673bc
gkkary3 <nambawon1@naver.com>
2025-03-26 22:32:55 +0900

    parallel router  commit

---
+++ section08/src/app/book/[id]/page.tsx
@@ -0,0 +1,81 @@
+import { notFound } from "next/navigation";
+import style from "./page.module.css";
+import { ReviewData } from "@/types";
+import ReviewItem from "@/components/review-item";
+import ReviewEditor from "@/components/review-editor";
+
+//false 시  generateStaticParams안에 있는 param외에 404 Page로 보내버림.
+export const dynamicParmas = false;
+// 정적인 param을 빌드 타임에 만들어내는 기능
+export function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
+
+async function BookDetail({ bookId }: { bookId: string }) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`,
+    { cache: "force-cache" }
+  );
+
+  if (!response.ok) {
+    if (response.status === 404) {
+      notFound();
+    }
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const book = await response.json();
+  const { id, title, subTitle, description, author, publisher, coverImgUrl } =
+    book;
+
+  return (
+    <section>
+      <div
+        className={style.cover_img_container}
+        style={{ backgroundImage: `url('${coverImgUrl}')` }}
+      >
+        <img src={coverImgUrl} />
+      </div>
+      <div className={style.title}>{title}</div>
+      <div className={style.subTitle}>{subTitle}</div>
+      <div className={style.author}>
+        {author} | {publisher}
+      </div>
+      <div className={style.description}>{description}</div>
+    </section>
+  );
+}
+
+async function ReviewList({ bookId }: { bookId: string }) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`,
+    { next: { tags: [`review-${bookId}`] } }
+  );
+
+  if (!response.ok) {
+    throw new Error(`Review fetch faild : ${response.statusText}`);
+  }
+  const reviews: ReviewData[] = await response.json();
+  return (
+    <section>
+      {reviews.map((review) => (
+        <ReviewItem key={`review-item-${review.id}`} {...review} />
+      ))}
+    </section>
+  );
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const param = await Promise.resolve(params); // `params`를 비동기적으로 해석
+
+  return (
+    <div className={style.container}>
+      <BookDetail bookId={param.id} />
+      <ReviewEditor bookId={param.id} />
+      <ReviewList bookId={param.id} />
+    </div>
+  );
+}
